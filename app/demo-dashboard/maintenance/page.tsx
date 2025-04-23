89620838b9c04d2ad61db9cf7702e8876488f0d1
Mohammed Hesham <144591912+Mohammed-HeshamMohammed@users.noreply.github.com>
2025-04-23 23:55:50 +0200

    lol

--- app/demo-dashboard/maintenance/page.tsx
+++ app/demo-dashboard/maintenance/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MaintenanceDashboard } from "@/components/demo-dashboard/MaintenanceDashboard/MaintenanceDashboard";
+import MaintenanceDashboard from "@/components/demo-dashboard/sections/Maintenance/MaintenanceDashboard";
 
 export default function MaintenanceDashboardPage() {
   return <MaintenanceDashboard />;
